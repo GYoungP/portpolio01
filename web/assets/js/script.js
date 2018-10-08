97fa5959980053e03e9aece12a24365da87449de
GyounGP <wldud45@naver.com>
2018-10-08 23:18:56 +0900

    HTMl/CSS 수정

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -16,53 +16,83 @@ var myApp = {
         myApp.TopBtn();
     },
     header: function () {
-        var gnb = $('.nav li');
-        var rMenu = $('.r-menu-item');
+        // var gnb = $('.nav li');
+        // var rMenu = $('.r-menu-item');
         var header = $('header');
-        var item = $('.item');
 
         $(window).on('scroll',function () {
             var sct = $(window).scrollTop();
 
-            if(sct > 100) {
+            if(sct > 500) {
                 header.addClass('is-active');
             }else {
                 header.removeClass('is-active');
             }
         });
 
-        gnb.hover(function () {
-            $(this).find('.dropdown').stop().slideDown(600);
-        },function () {
-            $(this).find('.dropdown').stop().hide();
-        });
-
-        rMenu.hover(function () {
-            $(this).find('.drop-menu').stop().slideDown(600);
-        },function () {
-            $(this).find('.drop-menu').stop().slideUp(300);
-        });
-
-        item.hover(function () {
-            header.addClass('is-active');
-        })
-
-
     },
 
     main: function () {
 
 
-        $('.section').on('mousewheel', function (event,delta) {
+        $('.main-visual').slick({
+            dots: true,
+            arrows:false,
+            infinite: true,
+            speed: 2000,
+            fade: true,
+            cssEase: 'linear',
+            autoplay:true,
+            autoplayspeed: 1000
+        });
+
+        AOS.init({
+            duration: 1000,
+            offset: 0,
+            delay: 100,
+            easing: 'easeInOutBack',
+        });
+
+
+        $('.main-visual-section').on('mousewheel', function (event,delta) {
             if (delta > 0) {
                 var prev = $(this).prev().offset().top;
 
                 $('html,body').stop().animate({'scrollTop': prev}, 1000, 'easeInQuart');
             } else if (delta < 0) {
                 var next = $(this).next().offset().top;
-                $('html,body').stop().animate({'scrollTop': next}, 1000, 'easeInQuart');
+                $('html,body').stop().animate({'scrollTop': next - 80}, 1000, 'easeInQuart');
             }
         });
+
+        var mainSection = $('.main-section');
+
+
+        $(window).on('scroll',function () {
+            var sct =$(window).scrollTop();
+
+            mainSection.each(function () {
+                var self = $(this),
+                    mainSectionOft = self.offset().top;
+
+                if (sct > mainSectionOft - 100) {
+                    self.addClass('is-active').siblings().removeClass('is-active');
+                }
+            });
+
+        });
+
+        const menu = $('.portfolio-btn');
+        const content = $('.portfolio-work-items');
+
+        menu.click(function () {
+            var self = $(this);
+            self.addClass('on').siblings().removeClass('on')
+
+
+            var idx = self.index();
+            content.eq(idx).fadeIn(600).addClass('is-active').siblings().fadeOut(600).removeClass('is-active');
+        }).eq(0).trigger('click');
 
     },
 
